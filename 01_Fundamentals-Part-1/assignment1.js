0d7278d9035e83bd3ab53f9694b1c75ede0c2853
DISHA-6 <dishamakal847@gmail.com>
2025-07-07 21:37:27 +0530

    First commit

---
+++ 01_Fundamentals-Part-1/assignment1.js
@@ -0,0 +1,7 @@
+let country="India";
+let continent="Asia";
+let population="1.46 Billion";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01_Fundamentals-Part-1/script.js
@@ -0,0 +1,9 @@
+let js="amazing";
+//if(js === 'amazing') alert('Javascript is FUN!');
+//40+8+23-10;
+console.log(40+8+23-10);
+console.log("Disha");
+console.log(23);
+let firstName="Disha";
+console.log(firstName);
+let PI=3.1415;//constant value thats why its written in caps
