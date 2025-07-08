9d76a7f878216fdeeda4be1c5d714542024d161b
DISHA-6 <dishamakal847@gmail.com>
2025-07-08 19:30:27 +0530

    Second Commit

--- 01_Fundamentals-Part-1/script.js
+++ 01_Fundamentals-Part-1/script.js
@@ -1,4 +1,4 @@
-let js="amazing";
+/**let js="amazing";
 //if(js === 'amazing') alert('Javascript is FUN!');
 //40+8+23-10;
 console.log(40+8+23-10);
@@ -6,4 +6,43 @@ console.log("Disha");
 console.log(23);
 let firstName="Disha";
 console.log(firstName);
-let PI=3.1415;//constant value thats why its written in caps
+let PI=3.1415;//constant value thats why its written in caps**/
+//true;
+/**let javascriptIsFun=true;
+console.log(javascriptIsFun);
+console.log(typeof true);// will show the datatype
+console.log(typeof javascriptIsFun );
+console.log(typeof 'Disha');
+console.log(typeof 23);
+let year;
+console.log(year);
+console.log(typeof year);
+year=1991;
+console.log(typeof year);
+console.log(typeof null);**/
+/*const firstName='Disha';
+const job= 'intern';
+const birthYear=2003;
+const year=2025;
+const disha="I'm " + firstName + ', a ' + (year - birthYear)
+ + ' years old ' + job +'!';
+ console.log(disha);
+const dishaNew= `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(dishaNew);
+console.log(`Just a regular string...`);*/
+const age=10;
+//const isOldEnough=age >=18;
+if(age >= 18){
+    console.log('Sarah can have driving license !');
+} else {
+    const yearsLeft = 18 - 10;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+const birthYear = 2003;
+let century;
+if(birthYear <= 2000){
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
